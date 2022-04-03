9173ba46a8a5ca793c5b9e4e8ac3295eec67be5b
kristianguevara <kristian.guevara@live.com>
2022-04-03 14:39:45 +0700

    Updated script to support browsers blocking data

--- js/script.js
+++ js/script.js
@@ -1,48 +1,69 @@
 $(document).ready(() => {
-    resetScreen()
+    resetScreen();
     checkIfSocial();
-})
+});
 
 function changeScreen(sel) {
-    localStorage.setItem("screen", sel.value)
-    resetScreen()
+  setLocalResolution(sel.value);
+}
+
+function setLocalResolution(value) {
+  try {
+    localStorage.setItem("screen", value);
+     resetScreen();
+  } catch(err) {
+    // Manually set screen if localStorage is now allowed on permissions
+    if(value === 'phone') {
+      setMobileScreen();
+      refreshIframe();
+    } else {
+      setFullScreen();
+      refreshIframe();
+    }
+  }
 }
 
 function setFullScreen() {
-    $("select").val("full")
-    $("#phone").removeClass("smartphone")
-    $("#phone").addClass("full-screen")
-    $("#content-container").addClass("full-screen")
+    $("select").val("full");
+    $("#phone").removeClass("smartphone");
+    $("#phone").addClass("full-screen");
+    $("#content-container").addClass("full-screen");
 }
 
 function setMobileScreen() {
-    $("select").val("phone")
-    $("#phone").removeClass("full-screen")
-    $("#content-container").removeClass("full-screen")
-    $("#phone").addClass("smartphone")
+    $("select").val("phone");
+    $("#phone").removeClass("full-screen");
+    $("#content-container").removeClass("full-screen");
+    $("#phone").addClass("smartphone");
 }
 
 function refreshIframe() {
-    document.getElementById("frame").srcdoc += ""
+    document.getElementById("frame").srcdoc += "";
 }
 
 function resetScreen() {
-    const screen = localStorage.getItem("screen")
-    if (screen) {
-        if (screen === "phone") {
-            setMobileScreen()
-            refreshIframe()
-        } else {
-            setFullScreen()
-            refreshIframe()
-        }
-    } else {
-        localStorage.setItem("screen", "phone")
-        resetScreen()
+
+    try {
+      const screen = localStorage.getItem("screen");
+      if (screen) {
+          if (screen === "phone") {
+              setMobileScreen();
+              refreshIframe();
+          } else {
+              setFullScreen();
+              refreshIframe();
+          }
+      } else {
+          setLocalResolution("phone");
+      }
+    }
+    catch(err) {
+      // iFrames cannot be accessed by other browser's localStorage hence why the height and width
+      // are cut from other computers. Must set to full screen if access to browser data is not permitted 
+      setMobileScreen();
+      refreshIframe();
     }
 }
-
-//TODO: Write code to disable the phone vs full dropdown and just have it full screen when there is a query paramater
 
 function checkIfSocial() {
     const urlSearchParams = new URLSearchParams(window.location.search);
@@ -51,5 +72,6 @@ function checkIfSocial() {
     if(params.share && params.share === '1') {
         $('#screen_type').remove();
         setFullScreen();
+        refreshIframe();
     }
 }
--- sketch.py
+++ sketch.py
@@ -1,40 +1,9 @@
 from p5 import *
 
-
-
-
-
 def setup():
   createCanvas(windowWidth,windowHeight)
-  background("red")
-  drawTickAxes()
-
-
-
+  background("black")
 
 
 def draw():
-
-  global x
-  global y
-  #background("black")
-
-  #drawTickAxes()
-  #fill("black")
-  #triangle(100,100,200,300,300,100)
-
-  #noStroke()
-
-  #random(0,255)
-  fill(random(0,0),random(0,0),random(0,255))
-  circle(mouseX,mouseY,80)
-
-
-  #x = x + 5
-  #y = y - 3
-
-
-
-
-
-
+  drawTickAxes()
